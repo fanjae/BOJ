9000b408a5a5d757c0e0d945780393a041b56da9
fanjaekim <hjjang45@gmail.com>
2020-10-11 16:58:30 +0900

    11719.c

--- I_기본문법/11719/11719.c
+++ I_기본문법/11719/11719.c
@@ -3,9 +3,9 @@ int main()
 {
 	int i = 0;
 	char data[105][105];
-	while(gets(data[i]) != NULL)
+	while(gets(data[i]) != NULL) // gets는 scanf와 다르게 space도 입력 받을 수 있다.
 	{
-		printf("%s\n",data[i]);
+		puts(data[i]); // printf와 다르게 자동 개행('\n')이 일어남.
 		i++;
 	}
 }
