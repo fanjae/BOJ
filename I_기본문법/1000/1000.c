c2c5a65b51e55e35822a743627387e4ba958a839
fanjaekim <hjjang45@gmail.com>
2020-10-11 15:59:43 +0900

    1000.c

--- I_기본문법/1000/1000.c
+++ I_기본문법/1000/1000.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	int a,b;
-	scanf("%d %d",&a,&b);
-	printf("%d",a+b);
+	int a,b; // 두 정수 선언
+	scanf("%d %d",&a,&b); // 십진 정수 입력.
+	printf("%d",a+b); // a+b 값 출력
 }
