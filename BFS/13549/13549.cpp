7a5a27a9a5f686d8635edd65c2699e07c3eec616
fanjaekim <hjjang45@gmail.com>
2021-08-03 20:22:46 +0900

    BOJ 13549 숨바꼭질 3 풀이
    13549 숨바꼭질 3 풀이(BFS)

--- BFS/13549/13549.cpp
+++ BFS/13549/13549.cpp
@@ -10,10 +10,10 @@ BFS(Breadth first search)
 
 using namespace std;
 
-int start_point,end_point;
-int moving[100005];
+int start_point,end_point; // 수빈 위치, 동생 위치 
+int moving[100005]; // 도달하는데 걸린 시간 
 
-bool safe(int x)
+bool safe(int x) // 좌표를 벗어나는 경우 확인. 
 {
 	if(x < 0 || x > 100000)
 	{
@@ -25,9 +25,9 @@ void bfs()
 {
 	queue <int> que;
 	que.push(start_point);
-	moving[start_point] = 0;
+	moving[start_point] = 0; // 초기 위치는 0초안에 갈 수있음  
 	
-	while(!que.empty())
+	while(!que.empty()) // 큐가 비어있지 않으면 반복 
 	{
 		int point = que.front();
 		que.pop();
@@ -37,19 +37,18 @@ void bfs()
 			printf("%d\n",moving[end_point]);
 			return ;
 		}
-		if(safe(point*2) == true && moving[point*2] == -1)
+		if(safe(point*2) == true && moving[point*2] == -1) // 순간이동은 0초 후에 2*x  위치 이동 
 		{
 			
 			que.push(point*2);
 			moving[point*2] = moving[point];
-			printf("point*2 : %d %d\n",point,moving[point]);
 		}
-		if(safe(point-1) == true && moving[point-1] == -1)
+		if(safe(point-1) == true && moving[point-1] == -1) // 1초 후에 x-1 위치로 이동 
 		{
 			que.push(point-1);
 			moving[point-1] = moving[point] + 1;
 		}	
-		if(safe(point+1) == true && moving[point+1] == -1)
+		if(safe(point+1) == true && moving[point+1] == -1) // 1초 후에 x+1 위치로 이동 
 		{
 			que.push(point+1);
 			moving[point+1] = moving[point] + 1;
